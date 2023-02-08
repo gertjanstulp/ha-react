f5749487243b2c0d334b47e917aba1d719fc3767
Gertjan <gertjanstulp@gmail.com>
2023-02-08 18:02:59 +0100

    Added input_boolean transform

---
+++ custom_components/react/tasks/transform/transform_input_boolean_changes.py
@@ -0,0 +1,26 @@
+""""Store React data."""
+from __future__ import annotations
+
+from custom_components.react.base import ReactBase
+from custom_components.react.tasks.transform_base import BinaryStateData, StateChangedEvent, StateData, StateTransformTask
+from custom_components.react.const import (
+    INPUT_BOOLEAN, 
+    INPUT_BOOLEAN_PREFIX,
+)
+
+
+async def async_setup_task(react: ReactBase) -> Task:
+    """Set up this task."""
+    return Task(react=react)
+
+
+class Task(StateTransformTask):
+    """ "React task base."""
+
+    def __init__(self, react: ReactBase) -> None:
+        super().__init__(react, INPUT_BOOLEAN_PREFIX, INPUT_BOOLEAN)
+        self.can_run_disabled = True
+
+
+    def read_state_data(self, event: StateChangedEvent) -> StateData:
+        return BinaryStateData(INPUT_BOOLEAN_PREFIX, event.payload)
