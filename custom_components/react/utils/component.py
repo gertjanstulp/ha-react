0887efc68cddb38da81b25715ddb9115f648ddc8
Gertjan <gertjanstulp@gmail.com>
2023-06-17 19:26:31 +0200

    Added component logging

--- custom_components/react/utils/component.py
+++ custom_components/react/utils/component.py
@@ -2,14 +2,13 @@ from homeassistant.const import SERVICE_RELOAD, SERVICE_TOGGLE, SERVICE_TURN_OFF
 from homeassistant.core import ServiceCall, callback
 from homeassistant.helpers.entity_component import EntityComponent
 from homeassistant.helpers.service import async_register_admin_service
+from custom_components.react import WorkflowEntity
 
-from .. import WorkflowEntity
-from ..base import ReactBase
+from custom_components.react.base import ReactBase
+from custom_components.react.const import DOMAIN, SERVICE_TRIGGER_WORKFLOW
+from custom_components.react.utils.logger import get_react_logger
 
-from ..const import (
-    DOMAIN, 
-    SERVICE_TRIGGER_WORKFLOW
-)
+_LOGGER = get_react_logger()
 
 
 async def async_setup_component(react: ReactBase):
@@ -24,7 +23,6 @@ async def async_setup_component(react: ReactBase):
         await entity.async_trigger(service_call.context)
     component.async_register_entity_service(SERVICE_TRIGGER_WORKFLOW, {}, trigger_workflow_service_handler)
 
-
     # Load workflow entities
     async def async_load():
         entities = []
@@ -32,11 +30,13 @@ async def async_setup_component(react: ReactBase):
             entities.append(WorkflowEntity(workflow, react))
         if entities:
             await component.async_add_entities(entities)
-    await async_load()
 
+    await async_load()
 
     # Enable reload option
     async def reload_service_handler(service_call: ServiceCall) -> None:
+        _LOGGER.debug("Reloading React configuration")
+
         # Unload workflow entities from component and reread configuration from file
         conf = await component.async_prepare_reload()
         if conf is None:
@@ -54,9 +54,7 @@ async def async_setup_component(react: ReactBase):
             }
         )
 
-
         # Load new workflow entities
         await async_load()
-
 
     async_register_admin_service(react.hass, DOMAIN, SERVICE_RELOAD, reload_service_handler)
